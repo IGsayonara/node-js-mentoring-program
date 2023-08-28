ee79e4141e8092ec790b60b12a6f35b740d3c238
Ihor <diduniki@gmail.com>
2023-08-28 19:03:26 +0300

    add REPL context add getRandomNumber function to REPL context

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log(123);
+const repl = require('repl');
+
+function getRandomNumber() {
+    return Math.random()
+}
+
+function initializeContext(context) {
+    context.getRandomNumber = getRandomNumber;
+}
+
+const replServer = repl.start();
+initializeContext(replServer.context);
+
+replServer.on('reset', initializeContext);
