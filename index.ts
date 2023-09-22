4168d5da1a6e1eebb0e6f4a2ca205397ca0d5860
Ihor <diduniki@gmail.com>
2023-09-22 12:17:01 +0300

    add e2e tests

--- index.ts
+++ index.ts
@@ -1,4 +1,10 @@
-import {getListOfPublicHolidays} from "./task4/src/services/public-holidays.service.ts";
+import {
+    checkIfTodayIsPublicHoliday,
+    getListOfPublicHolidays,
+    getNextPublicHolidays
+} from "./task4/src/services/public-holidays.service.ts";
 
 
 getListOfPublicHolidays(2023, 'DE').then(console.log)
+// getNextPublicHolidays('DE').then(console.log)
+// checkIfTodayIsPublicHoliday('DE').then(console.log)
