7403283ac883ccc9c0cda403bf02f64baf5697b7
Ihor <diduniki@gmail.com>
2023-10-13 13:17:52 +0300

    task7: add seeders for products

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,6 +1,7 @@
 import { DataSourceOptions } from 'typeorm';
+import { SeederOptions } from 'typeorm-extension';
 
-const postgresOptions: DataSourceOptions = {
+const postgresOptions: DataSourceOptions & SeederOptions = {
   type: 'postgres',
   host: 'localhost',
   port: 5432,
@@ -8,7 +9,9 @@ const postgresOptions: DataSourceOptions = {
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   entities: ['src/entities/*.ts'],
-  migrations: ['src/migrations/*.ts'],
+  migrations: ['src/database/migrations/*.ts'],
+  seeds: ['src/database/seeds/**/*{.ts,.js}'],
+  factories: ['src/database/factories/**/*{.ts,.js}'],
   synchronize: process.env.NODE_ENV !== 'prod',
 };
 
