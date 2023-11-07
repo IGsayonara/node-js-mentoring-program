af9aeea7cd97de8630e99c81dcbebe711b578d5c
Ihor <diduniki@gmail.com>
2023-11-07 12:27:47 +0200

    task10: add health endpoint

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -4,12 +4,14 @@ import { cartRouter } from './cart.route.ts';
 import { userAuth } from '../middleware/user-auth.middleware.ts';
 import { formatResponse } from '../middleware/response.middleware.ts';
 import { authRouter } from './auth.route.ts';
+import { healthCheck } from '../controllers/health.controller.ts';
 
 export const appRouter = Router();
 
 appRouter.use('/profile', userAuth);
 appRouter.use('/', formatResponse);
 
+appRouter.get('/health', healthCheck);
 appRouter.use('/auth', authRouter);
 appRouter.use('/products', productRouter);
 appRouter.use('/profile/cart', cartRouter);
