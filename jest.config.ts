ff06463ae81804ff60f39d401756932433884b16
Ihor <diduniki@gmail.com>
2023-11-20 23:23:38 +0200

    task10: test pre commit hook

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,14 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    roots: ['./task4/src'],
-    silent: false,
-    verbose: true,
-    collectCoverageFrom: ['./task4/src/**'],
-    coverageReporters: ['text'],
-    coverageThreshold: {
-        global: {
-            lines: 90
-        }
-    }
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['./src'],
+  silent: false,
+  verbose: true,
+  collectCoverageFrom: ['./task4/src/**'],
+  coverageReporters: ['text'],
+  coverageThreshold: {
+    global: {
+      lines: 90,
+    },
+  },
 };
