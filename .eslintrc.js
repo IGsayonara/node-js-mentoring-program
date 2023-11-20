955d719e335d1769976e631ee69fb6a2bbdb3b53
Ihor <diduniki@gmail.com>
2023-11-20 23:38:23 +0200

    task10: test lint-fix hook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,11 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'prettier',
-    ],
-    plugins: ['@typescript-eslint', 'prettier'],
-    rules: {
-        'prettier/prettier': 'error',
-    },
-    env: {
-        node: true
-    },
+  parser: '@typescript-eslint/parser',
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+  env: {
+    node: true,
+  },
 };
